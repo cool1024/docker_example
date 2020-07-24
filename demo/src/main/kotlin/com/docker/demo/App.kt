509ca8e3fb17ea75d42cea88f861cc9a5ecd3127
cool1024 <xiaojiannangkang@126.com>
2020-07-24 17:26:03 +0800

    update demo

--- demo/src/main/kotlin/com/docker/demo/App.kt
+++ demo/src/main/kotlin/com/docker/demo/App.kt
@@ -3,21 +3,26 @@ package com.docker.demo
 import com.docker.demo.config.ZookeeperConfig
 import com.docker.demo.prop.RunTimeProp
 import org.apache.zookeeper.ZooKeeper
+import org.slf4j.LoggerFactory
+
 
 object App {
 
     private val zookeeperConfig = RunTimeProp.config<ZookeeperConfig>()!!
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     @JvmStatic
     fun main(args: Array<String>) {
         println("App run...")
 
-        createZookeeperHandle()
+        val zooKeeper = createZookeeperHandle()
+        Thread.sleep(6000)
+        zooKeeper.close()
     }
 
-    private fun createZookeeperHandle() {
-        ZooKeeper(zookeeperConfig.connectString, zookeeperConfig.sessionTimeOut) { event ->
-            print(event)
+    private fun createZookeeperHandle(): ZooKeeper {
+        return ZooKeeper(zookeeperConfig.connectString, zookeeperConfig.sessionTimeOut) { event ->
+            logger.info("watcher event => [{}]", event)
         }
     }
 }
