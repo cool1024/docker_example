2842415f93ae1cda2ddde7f78975e1faefc0a565
cool1024 <xiaojiannangkang@126.com>
2021-01-19 14:54:21 +0800

    add config

--- resource/skywalking/install.sh
+++ resource/skywalking/install.sh
@@ -8,12 +8,28 @@ WORK_RESOURCE=/var/skywalking/resource
 VERSION=8.3.0
 DOWNLOAD_URL=https://mirror.bit.edu.cn/apache/skywalking/${VERSION}/apache-skywalking-apm-${VERSION}.tar.gz
 RESOURCE_NAME=apache-skywalking-apm-${VERSION}.tar.gz
+LIB_PATH=/var/skywalking/resource/apache-skywalking-apm-bin
 
 function download_skywalking() {
     $TARGET_FILE=/var/skywalking/resource/$RESOURCE_NAME
     if [[ ! -f "$TARGET_FILE" ]]; then
         curl $DOWNLOAD_URL -o $TARGET_FILE
+        tar -xzvf $TARGET_FILE
+        cp -f $WORK_RESOURCE/application.yaml $LIB_PATH/conf
+        cp -f $WORK_RESOURCE/webapp.yaml $LIB_PATH/webapp
     fi
 }
 
+function start_ui_server() {
+    cd $LIB_PATH
+    ./bin/oapService.sh
+    ./bin/webappService.sh
+}
+
+function start_agent() {
+    -Dskywalking.agent.application_code=dubbo-consumer -Dskywalking.collector.servers=localhost:10800
+    java -javaagent:$LIB_PATH/skywalking-agent/skywalking-agent.jar -jar app.jar
+}
+
 download_skywalking
+start_ui_server
